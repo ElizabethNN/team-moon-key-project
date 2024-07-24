7a165a2b193dce909035e5f9b4be226269f190d8
adamoni123 <68234471+adamoni123@users.noreply.github.com>
2024-07-24 23:41:58 +0300

    transformers

--- Cat.gd
+++ Cat.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-class_name Player
 
-var speed: float
+
+var walking_speed: float
+@export
+var climing_speed: float
 @export
 var max_speed: float
 @export
@@ -13,15 +15,19 @@ var acceleration: float
 var deceleration: float
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var double_jump = false
 var wall_jump = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	if not double_jump and is_on_floor():
+		double_jump = true;
 
 	# Handle jump.
-	if Input.is_action_just_pressed("movement_up") and (is_on_floor() or wall_jump):
+	if Input.is_action_just_pressed("movement_up") and (is_on_floor() or double_jump or wall_jump):
+		double_jump = false if not is_on_floor() else double_jump
 		wall_jump = false
 		velocity.y = -jump_speed
 
@@ -29,19 +35,21 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("movement_left", "movement_right")
 	if direction:
-		speed += acceleration
-		velocity.x = direction * speed
+		walking_speed += acceleration
+		velocity.x = direction * walking_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, deceleration)
 	
-	if speed > max_speed:
-		speed = max_speed
+	if walking_speed > max_speed:
+		walking_speed = max_speed
 	
 	if Input.is_action_pressed("grab_wall") and is_on_wall():
-		velocity.y = 0
+		if Input.is_action_pressed("movement_up"):
+			velocity.y = -climing_speed
+		else:
+			velocity.y = 0
 		wall_jump = true
 	
 	
 
 	move_and_slide()
-
