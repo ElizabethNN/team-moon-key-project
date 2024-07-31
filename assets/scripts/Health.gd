1ed66339c2ef3253f3c9829875413bd998c7c37e
ElizabethNN <wotik172002@gmail.com>
2024-07-31 09:45:58 +0300

    Health bar fix

--- assets/scripts/Health.gd
+++ assets/scripts/Health.gd
@@ -15,6 +15,8 @@ signal health_changed(old_value, new_value)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_health = max_health
+	health_bar.max_value = max_health
+	health_bar.value = current_health
 
 func change_health(delta: int):
 	health_bar.value = current_health
