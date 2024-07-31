63c9055deeef40df5512bfcdb340f6ae7604b30c
adamoni123 <68234471+adamoni123@users.noreply.github.com>
2024-07-31 09:31:25 +0300

    broken code

--- assets/scripts/Health.gd
+++ assets/scripts/Health.gd
@@ -7,6 +7,9 @@ var max_health: int
 
 var current_health: int
 
+@onready
+var health_bar = $"../HealthBar"
+
 signal health_changed(old_value, new_value)
 
 # Called when the node enters the scene tree for the first time.
@@ -14,12 +17,19 @@ func _ready():
 	current_health = max_health
 
 func change_health(delta: int):
+	health_bar.value = current_health
 	if not timer.is_stopped():
 		return
 	emit_signal("health_changed", current_health, current_health + delta)
 	current_health += delta
 	print(get_parent().name + ": health is " + str(current_health))
 	timer.start()
+	
+
+	if current_health >= 6:
+		health_bar.visible = false
+	else:
+		health_bar.visible = true
 
 func _on_invincibility_timer_timeout():
 	timer.stop()
